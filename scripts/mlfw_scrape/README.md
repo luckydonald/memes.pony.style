85dd5cea9e1a06b5db45bcfd64e2145b95210312
rockybulwinkle <rockybulwinkle@gmail.com>
2019-07-13 22:36:33 -0500

    Initial commit of scraper. Just collects the data, does not yet save anything but the html anywhere.

---
+++ scripts/mlfw_scrape/README.md
@@ -0,0 +1,3 @@
+Script to download all the mylittlefacewhen pages listed in mlfw.json and extract the tags.
+
+mlfw.json acquired from the javascript variables from archive.org search results page for mylittlefacewhen using firefox's dev tools.
---
+++ scripts/mlfw_scrape/scrape.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+import os
+import datetime
+import json
+import subprocess
+import re
+from filelock import FileLock
+from collections import namedtuple
+from html.parser import HTMLParser
+
+face_tuple = namedtuple('Face', ['url', 'mime', 'begin', 'end', 'groupcnt', 'uniqcnt'])
+
+image_re = re.compile(".*mylittlefacewhen.com(:80)?/f/[1-9][0-9]*$")
+
+tag_re = re.compile(".*http://mylittlefacewhen.com(:80)?/search/\?tag=.*")
+
+with open("mlfw.json") as f:
+    r = f.read()
+
+
+dump = json.loads(r)
+
+try:
+    os.mkdir("mlfw_scrape")
+except FileExistsError as e:
+    pass
+
+
+class MyHTMLParser(HTMLParser):
+    """
+    Parser for the html. It searches for the "a" of class "tag" and saves what it finds to an instance variable.
+    """
+    def __init__(self):
+        super().__init__()
+        self.last_tag = None
+        self.last_attrs = None
+
+    def handle_starttag(self, tag, attrs):
+        self.last_tag = tag
+        self.last_attr = attrs
+
+    def handle_endtag(self, tag):
+        self.last_tag = None
+        self.last_attr = None
+
+    def handle_data(self, data):
+        if self.last_tag == "a":
+            self.last_attr = {k:v for k,v in self.last_attr}
+            if "class" in self.last_attr  and self.last_attr["class"] == "tag" and tag_re.match(self.last_attr["href"]):
+                self.last_attr = None
+                self.last_tag = None
+                self.tags.append(data)
+
+    def feed(self, data):
+        self.tags = list()
+        super().feed(data)
+        return self.tags
+
+parser = MyHTMLParser()
+
+
+
+with open("error_log", "a") as error_log:
+    print("Run starting %s:"%datetime.datetime.now(), file=error_log)
+    for idx,face in enumerate(dump):
+        face = face_tuple(*face)
+        print ('getting', face)
+        url = "https://web.archive.org/web/%(date)s/%(url)s"%dict(date=face.begin, url=face.url)
+        print(url)
+        #download the page
+        try:
+            o = subprocess.check_output(["curl", url])
+        except subprocess.CalledProcessError as e:
+            print("%s: error on face id %d %s"%(e, idx, face), file=error_log)
+            continue
+        #make a directory for the output and save it
+        dir_ = "mlfw_scrape/%s/"%(idx)
+        try:
+            os.mkdir(dir_)
+        except FileExistsError:
+            pass
+
+        with open(dir_+"foo.html", "wb") as f:
+            f.write(o)
+
+        #check to see if it is a face url
+        if not image_re.match(face.url):
+            print("ignoring", face)
+            continue
+        #parse the html
+        o = o.decode("utf-8")
+        tags = parser.feed(o)
